c8c193041917d8f33219ab1f3f5f7a88a4e933e5
JamesMasterman <masterman.james@gmail.com>
2021-09-05 10:14:18 +1000

    set system to auto

--- src/irtemp.ino
+++ src/irtemp.ino
@@ -4,18 +4,17 @@
 
 //Thingsboard settings
 const char THINGSBOARD_SERVER[] = "192.168.1.8";
-const char DeviceAttributes[] = "{\"firmware_version\":\"1.5.2\",\"software_version\":\"1.0\"}";
+const char DeviceAttributes[] = "{\"firmware_version\":\"2.1.0\",\"software_version\":\"1.1\"}";
 #define THINGSBOARD_PORT 1883
 #define TOKEN "nO8pt1o9xxt43FqMKsDK"
 
 const unsigned long ONE_MIN_MS = 60*1000;
 const unsigned long WATCHDOG_TIMEOUT_MS = 15*ONE_MIN_MS; //timeout for watchdog
 const unsigned long LOOP_TIME_MS= 30000; //30 second send interval
-const unsigned long WIFI_TIMEOUT_MS = 30*1000; //30 second timeout waiting for the wifi
 
 STARTUP(WiFi.selectAntenna(ANT_INTERNAL));
-ApplicationWatchdog wd(WATCHDOG_TIMEOUT_MS, System.reset);
-SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
 
 TempLogger* logger;
 TempReader* reader;
@@ -25,31 +24,15 @@ unsigned long lastSend;
 void setup()
 {
     //Serial.begin(9600);
-    startWifi();
     reader = new TempReader();
     logger = new TempLogger();
     logger->Setup(THINGSBOARD_SERVER, THINGSBOARD_PORT, DeviceAttributes, TOKEN);
-}
-
-void startWifi()
-{
-  //Sync the time
-  if(!WiFi.ready())
-  {
-     WiFi.on();
-     WiFi.connect();
-     waitFor(WiFi.ready, WIFI_TIMEOUT_MS);
-  }
 }
 
 void loop()
 {
   unsigned long start = millis();
 
-  //Tell the watchdog we are still alive
-  wd.checkin();
-
-  startWifi();
   logger->Send(reader->GetTemp());
 
   //wait till next loop
